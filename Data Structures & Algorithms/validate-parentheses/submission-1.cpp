74a1eb0cf4df66b05ba3b22104e1f0bb45be93f8
Eason0604 <91084270+Eason0604@users.noreply.github.com>
2026-09-15 16:23:56 +0800

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> temp;
+        for(char& c:s){
+            if(c=='('||c=='{'||c=='['){
+                temp.push(c);
+            }else{
+                if(temp.empty()){
+                    return false;
+                }
+                if( (temp.top()=='('&&c==')')||(temp.top()=='{'&&c=='}')||(temp.top()=='['&&c==']') ){
+                    temp.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
