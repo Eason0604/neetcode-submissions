eee1d3ffa5a754312142d326cc40c0d5847fb743
Eason0604 <91084270+Eason0604@users.noreply.github.com>
2026-09-19 21:03:04 +0800

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n=position.size();
+        vector<pair<int,float>> v;
+        for(int i=0;i<n;i++){
+            v.emplace_back(position[i],float(target-position[i])/speed[i]);
+        }
+        sort(v.begin(),v.end());
+        float last_time=-1;
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            if(v[i].second>last_time){
+                ans++;
+                last_time=v[i].second;
+            }
+        }
+        return ans;
+    }
+};
